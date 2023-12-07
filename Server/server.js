f5b8ab50b5b81b8628d741c77684f5239bbd69c8
Ayush <84891062+Ayush945@users.noreply.github.com>
2023-12-07 08:31:26 +0545

    added two table

--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,10 @@ app.get('/', async (req, res) => {
     res.send("Welcome to Web App")
 })
 
+app.get('/get-items', async (req, res) => {
+    res.send(db.query('SELECT * FROM USER'))
+})
+
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
 })
